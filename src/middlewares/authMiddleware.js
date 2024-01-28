755d9ee974439503c428fef37ea2eaf1b306280e
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-01-28 13:47:26 +0600

    update

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -8,7 +8,7 @@ const authenticateJWT = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.TOKEN);
     req.user = decoded;
     next();
   } catch (error) {
